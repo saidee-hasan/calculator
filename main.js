882f8d79ef1534452ae6db8093d090a2e4e0d17c
Saide Hasan <mdsaidehasan12345@gmail.com>
2024-03-17 10:27:59 +0600

    calculetor

---
+++ main.js
@@ -0,0 +1,14 @@
+function DeleteMe(){
+    document.getElementById('myResult').value = ''
+
+}
+function calculator(NewValue){
+    document.getElementById('myResult').value += NewValue ;
+
+}
+function AnsWer(){
+    const a = document.getElementById('myResult').value;
+    const b = eval(a);
+    document.getElementById('myResult').value = b;
+
+}
