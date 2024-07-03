01a7da0f0ee1d5fcbb078193944692ddaaa6810c
saideeWD <mdsaideehasan@gmail.com>
2024-07-03 18:46:44 +0600

    fff

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-function DeleteMe(){
-    document.getElementById('myResult').value = ''
-
+function DeleteMe() {
+  document.getElementById("myResult").value = "";
 }
-function calculator(NewValue){
-    document.getElementById('myResult').value += NewValue ;
 
+function calculator(NewValue) {
+  document.getElementById("myResult").value += NewValue;
 }
-function AnsWer(){
-    const a = document.getElementById('myResult').value;
-    const b = eval(a);
-    document.getElementById('myResult').value = b;
-
+function AnsWer() {
+  const a = document.getElementById("myResult").value;
+  const b = eval(a);
+  document.getElementById("myResult").value = b;
 }
